d74a921a9d71420ae3d8e0c067b03f9ebf487703
Johan Euphrosine <proppy@gmail.com>
2025-11-06 01:27:42 +0800

    test/test: fix async comprehension

--- test/test.py
+++ test/test.py
@@ -22,7 +22,7 @@ async def collect_samples(dut, count):
 
 async def write_wavefile(dut):
     """Write a wavefile corresponding to the outputs."""
-    samples = [s async for i in collect_samples(dut, 65536*2)]
+    samples = [s async for s in collect_samples(dut, 65536*2)]
     df = pd.DataFrame.from_records(samples, columns=['t', 'out0'], index=['t'])
     df.plot()
     plt.savefig('tb.png')
