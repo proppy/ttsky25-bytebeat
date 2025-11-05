252692ecc687ec18ccea253aa7fda66ba4963b16
Johan Euphrosine <proppy@gmail.com>
2025-11-06 02:20:58 +0800

    test/test: longer sim

--- test/test.py
+++ test/test.py
@@ -26,7 +26,7 @@ async def collect_samples(dut, count):
 
 async def write_wavefile(dut):
     """Write a wavefile corresponding to the outputs."""
-    samples = [s async for s in collect_samples(dut, 65536*2)]
+    samples = [s async for s in collect_samples(dut, 65536*4)]
     df = pd.DataFrame.from_records(samples, columns=['t', 'out0'], index=['t'])
     df.plot()
     plt.savefig('tb.png')
@@ -46,14 +46,26 @@ async def test_bytebeat(dut):
 
     dut.ena.value = 1 # enable project
     dut.rst_n.value = 0 # low to reset
-    dut.ui_in.value = 0x57  # a=5 b=7
-    dut.uio_in.value = 0x3a # c=3 d=10
+    dut.ui_in.value = 0x75  # a=5 b=7
+    dut.uio_in.value = 0xa3 # c=3 d=10
     await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1 # take out of reset
     await ClockCycles(dut.clk, 65536)
     dut.rst_n.value = 0 # low to reset
-    dut.ui_in.value = 0x36  # a=3 b=6
-    dut.uio_in.value = 0x29 # c=2 d=9
+    dut.ui_in.value = 0x63  # a=3 b=6
+    dut.uio_in.value = 0x92 # c=2 d=9
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1 # take out of reset
+    await ClockCycles(dut.clk, 65536)
+    dut.rst_n.value = 0 # low to reset
+    dut.ui_in.value = 0x57  # a=7 b=5
+    dut.uio_in.value = 0x3a # c=10 d=3
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1 # take out of reset
+    await ClockCycles(dut.clk, 65536)
+    dut.rst_n.value = 0 # low to reset
+    dut.ui_in.value = 0x36  # a=6 b=3
+    dut.uio_in.value = 0x29 # c=9 d=2
     await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1 # take out of reset
     await ClockCycles(dut.clk, 65536)
