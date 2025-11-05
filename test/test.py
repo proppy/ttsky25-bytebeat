5ddbe44966b5fc94f9849feaf9f340e01f2f32ce
Johan Euphrosine <proppy@gmail.com>
2025-11-06 00:03:50 +0800

    test: add cocostb test

--- test/test.py
+++ test/test.py
@@ -1,40 +1,27 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# Copyright 2023 Google LLC.
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, RisingEdge
 
+FREQ = 8000
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+async def test_bytebeat(dut):
+    dut._log.info("start")
+    clock = Clock(dut.clk, (1.0/FREQ), units="sec")
     cocotb.start_soon(clock.start())
-
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
+    await RisingEdge(dut.clk) # wait for first clock edge
+    dut.rst_n.value = 0 # low to reset
+    dut.ui_in.value = 0x57  # a=5 b=7
+    dut.uio_in.value = 0x3a # c=3 d=10
     await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    dut.rst_n.value = 1 # take out of reset
+    await ClockCycles(dut.clk, 65536)
+    dut.rst_n.value = 0 # low to reset
+    dut.ui_in.value = 0x36  # a=3 b=6
+    dut.uio_in.value = 0x29 # c=2 d=9
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1 # take out of reset
+    await ClockCycles(dut.clk, 65536)
